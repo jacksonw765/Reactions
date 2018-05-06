41581595436febab8bd2d883d1b65dee77bd6e9b
welchjk <welchjk@mail.uc.edu>
2018-05-05 21:31:13 -0400

    first big update

---
+++ app/src/main/java/com/jacksonw765/reactions/HomeScreen.kt
@@ -0,0 +1,41 @@
+package com.jacksonw765.reactions
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.transition.Explode
+import android.widget.Button
+import android.transition.Slide
+import android.view.View
+import android.app.ActivityOptions
+import android.content.Intent
+import android.transition.Scene
+import android.transition.TransitionManager
+import android.view.ViewGroup
+
+
+class HomeScreen : AppCompatActivity() {
+
+    lateinit var buttonSettings: Button
+    lateinit var buttonStart: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar!!.hide()
+        setContentView(R.layout.activity_home_screen)
+        setupWindowAnimations()
+        buttonStart = findViewById(R.id.buttonStart)
+
+        buttonStart.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val i = Intent(applicationContext, MainActivity::class.java)
+               startActivity(i)
+            }
+        })
+    }
+
+    private fun setupWindowAnimations() {
+        val explode = Explode()
+        explode.duration = 1000
+        window.exitTransition = explode
+    }
+}
--- app/src/main/java/com/jacksonw765/reactions/MainActivity.kt
+++ app/src/main/java/com/jacksonw765/reactions/MainActivity.kt
@@ -2,14 +2,18 @@ package com.jacksonw765.reactions
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.RelativeLayout
+import android.os.CountDownTimer
+import android.os.Looper
 import java.util.*
 import android.util.DisplayMetrics
 import android.view.View
 import android.view.Window
-import android.widget.TextView
+import android.transition.Fade
+import android.widget.*
+import android.R.raw
+import android.media.MediaPlayer
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,51 +22,104 @@ class MainActivity : AppCompatActivity() {
     lateinit var layout: RelativeLayout
     lateinit var random: Random
     lateinit var textScore: TextView
+    lateinit var textCountDown: TextView
+
+    val displayMetrics = DisplayMetrics()
+
+    var isGameStarted: Boolean = false
     var score: Int = 0
+    var displayX = -1
+    var displayY = -1
+
+    var heightLow = -1.0
+    var withLow = -1.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         supportActionBar!!.hide()
-
         setContentView(R.layout.activity_main)
-
 
         layout = findViewById(R.id.layout)
         button = findViewById(R.id.imageView)
         textScore = findViewById(R.id.textScore)
+        textCountDown = findViewById(R.id.textCountdown)
         random = Random()
-        val displayMetrics = DisplayMetrics()
+
         windowManager.defaultDisplay.getMetrics(displayMetrics)
-        //displayMetrics.
 
-        val displayX = displayMetrics.widthPixels
-        val displayY = displayMetrics.heightPixels
-        println(displayX)
-        println(displayY)
+        displayX = displayMetrics.widthPixels
+        displayY = displayMetrics.heightPixels
 
-        val heightLow = displayY * .18
-        val withLow = displayX * .12
+        heightLow = displayY * .18
+        withLow =  displayX * .15
 
-        button.x = random.nextInt((displayMetrics.widthPixels)/2).toFloat()
-        button.y = random.nextInt((displayMetrics.heightPixels)/2).toFloat()
-        println("X is: " + button.x)
-        println("Y is: " + button.y)
+        button.x = -1000f
+        button.y = -1000f
 
+        setupTimer()
+    }
 
+    private fun startGame() {
+        //while(isGameStarted) {
+            button.x = random.nextInt((displayMetrics.widthPixels)/2).toFloat()
+            button.y = random.nextInt((displayMetrics.heightPixels)/2).toFloat()
+            println("X is: " + button.x)
+            println("Y is: " + button.y)
 
-        button.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(p0: View?) {
-                ++score
-                button.x = random.nextInt((displayX - withLow.toInt())) + withLow.toFloat()
-                button.y = random.nextInt((displayY - heightLow.toInt())) + withLow.toFloat()
-                println("X is: " + button.x)
-                println("Y is: " + button.y)
-                textScore.text = ""+score
+            button.setOnClickListener(object: View.OnClickListener {
+                override fun onClick(view: View?) {
+                    ++score
+                    var randomx = random.nextInt((displayX - withLow.toInt())) + withLow.toFloat()
+                    var randomy = random.nextInt((displayY - heightLow.toInt())) + withLow.toFloat()
+                    println("*****$randomx")
+                    println("*****$randomy")
+                    button.x = randomx
+                    button.y = randomy
+                    println("X is: " + button.x)
+                    println("Y is: " + button.y)
+                    textScore.text = ""+score
 
+                }
+            })
+        //}
+    }
+
+    private fun setupTimer() {
+        val mp = MediaPlayer.create(applicationContext, R.raw.countdown)
+        val delay: Long = 4000
+        val period: Long = 1000
+        val timer = object : CountDownTimer(delay, period) {
+                override fun onFinish() {
+                    startGameTimer()
+                }
+
+                override fun onTick(millisUntilFinished: Long) {
+                    if(millisUntilFinished < 1000) {
+                        textCountDown.text = ""+0
+                    }
+                    textCountDown.text = ""+(millisUntilFinished/1000).toInt()
+                }
+            }
+        timer.start()
+        mp.start()
+    }
+
+    private fun startGameTimer() {
+        val delay: Long = 1000
+        val period: Long = 25000
+        val timer = object : CountDownTimer(delay, period) {
+            override fun onFinish() {
+                isGameStarted = false
             }
 
-        })
+            override fun onTick(untilFinished: Long) {
+                textCountDown.text = "" + untilFinished
+
+            }
+        }
+        timer.start()
+        isGameStarted = true
+        startGame()
     }
+
 }
